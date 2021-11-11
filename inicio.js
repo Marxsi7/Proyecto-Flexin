9e076046568e208caec8d24ff6942d1ee76b1dd3
Marxsi7 <89942553+Marxsi7@users.noreply.github.com>
2021-11-11 17:17:41 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,16 @@
+function ir(){
+
+var c=1234;
+var u="bruno";
+
+if(document.form.password.value==c && document.form.login.value==u){
+alert ("Bienvenido bruno");
+window.location="tabla.html";}
+
+else{
+alert("porfavor ingrese, nombre y usuario correcto.");
+}
+
+
+
+}
